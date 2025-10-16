dedac60ecb38a500434550c01b6cf211cac38e25
CheckMat007 <gustavomagalhaes.caxambu@gmail.com>
2025-10-16 17:42:58 -0400

    auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,4 @@
-import NextAuth, { type NextAuthOptions } from "next-auth";
+import type { NextAuthOptions } from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaClient, Role } from "@prisma/client";
