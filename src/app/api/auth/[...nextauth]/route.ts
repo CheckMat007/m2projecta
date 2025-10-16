b3e68fd6f852caa5b7ae452f8268f609c18104a1
CheckMat007 <gustavomagalhaes.caxambu@gmail.com>
2025-10-16 05:44:41 -0400

    adicionado elsint.config.mjs e excluido temporariamente regra do any v2

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -60,6 +60,7 @@ export const authOptions: NextAuthOptions = {
     async session({ session, token }) {
       if (token && session.user) {
         session.user.id = token.id as string;
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         session.user.role = token.role as any;
       }
       return session;
